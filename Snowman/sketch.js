6937e7d200b3b572952a2769b93d0f8a47b00fdc
Charlie <48356556+mhsCMacDonald@users.noreply.github.com>
2019-05-09 10:25:23 -0400

    Update sketch.js

--- Snowman/sketch.js
+++ Snowman/sketch.js
@@ -1,25 +1,21 @@
-  //Title: Snowman. Finished 3/7/2019 by Cmacd1
+/* U4L4 - Snowman Drawing
+Finished by Charlie MacDonald on March 7th, 2019*/
+
 function setup() {
   createCanvas(400,400);
-  background(110, 168, 255);
+  background(110, 168, 255); //sky
 }
 
 function draw() {
-//sky
-
-
 //ground
   rect(0,350,399,49);
-  
-//snowman body
-  //bottom circle
+
+//snowman body, bottom to top
   ellipse(200, 300, 150, 150);
-  //middle circle
   ellipse(200, 200, 100, 100);
-  //top circle
   ellipse(200, 120, 75, 75);
 
-//snowman arms
+//snowman arms, left then right
   line(160,200,80,100);
   line(240,200,320,100);
 }
